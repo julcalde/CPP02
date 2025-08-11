4351bd394ded865314e2c4940758c1665c791801
julcalde <julcalde@student.42heilbronn.de>
2025-08-11 15:31:12 +0200

    fixed.hpp ready to deploy

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,27 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 15:08:08 by julcalde          #+#    #+#             */
-/*   Updated: 2025/08/11 15:08:09 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/08/11 15:18:22 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+	private:
+		int _value;
+		static const int _fracBits = 8; // number of fractional bits
+	
+	public:
+		Fixed();	// Default constructor
+		Fixed(const Fixed& other);	// Copy constructor
+		Fixed& operator=(const Fixed& other);	// Copy assignment operator
+		~Fixed();	//Destructor
+
+		int getRawBits() const;
+		void setRawBits(int const raw);
+};
+
+#endif
