729a2808113c74db0babcb4145d0fa9070eb0be2
julcalde <julcalde@student.42heilbronn.de>
2025-08-11 18:26:14 +0200

    copied the main.cpp content from subject again and edited to better readability. Also built the Fixed.hpp according to request. And it is not the painful part yet.

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 17:08:41 by julcalde          #+#    #+#             */
-/*   Updated: 2025/08/11 17:32:45 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/08/11 18:00:17 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,43 @@
 class Fixed
 {
 	private:
-		int _value;
-		static const int _fracBits = 8;
+		int _value;							// private variable to hold the fixed-point value
+		static const int _fracBits = 8;		// Number of fractional bits
 
 	public:
-		Fixed();	// Default constructor
-		Fixed(const int value);	// int constructor
-		Fixed(const float value);	// float constructor
-		Fixed(const Fixed& other);	// Copy constructor
+		Fixed();								// Default constructor
+		Fixed(const int value);					// int constructor
+		Fixed(const float value);				// float constructor
+		Fixed(const Fixed& other);				// Copy constructor
 		Fixed& operator=(const Fixed& other);	// Copy assignment operator
-		~Fixed();	//Destructor
+		~Fixed();								//Destructor
 
-		int getRawBits() const;
-		void setRawBits(int const raw);
-		float toFloat() const;	// Converts to float
-		int toInt() const;	// Converts to int
+		bool operator>(Fixed& other) const;		// Greater than operator
+		bool operator<(Fixed& other) const;		// Less than operator
+		bool operator>=(Fixed& other) const;	// Greater than or equal to operator
+		bool operator<=(Fixed& other) const;	// Less than or equal to operator
+		bool operator==(Fixed& other) const;	// Equality operator
+		bool operator!=(Fixed& other) const;	// Not equal operator
+
+		Fixed operator+(const Fixed& other) const;	// Addition operator
+		Fixed operator-(const Fixed& other) const;	// Subtraction operator
+		Fixed operator*(const Fixed& other) const;	// Multiplication operator
+		Fixed operator/(const Fixed& other) const;	// Division operator
+
+		Fixed& operator++();			// Pre-increment operator
+		Fixed& operator++(int);			// Post-increment operator
+		Fixed& operator--();			// Pre-decrement operator
+		Fixed& operator--(int);			// Post-decrement operator
+
+		static Fixed& min(Fixed& a, Fixed&b);						// Returns the minimum of two Fixed objects
+		static const Fixed& min(const Fixed& a, const Fixed&b);		// Returns the minimum of two Fixed objects
+		static Fixed& max(Fixed& a, Fixed&b);						// Returns the maximum of two Fixed objects
+		static const Fixed& max(const Fixed& a, const Fixed&b);		// Returns the maximum of two Fixed objects
+
+		int getRawBits() const;			// Returns the raw value
+		void setRawBits(int const raw);	// Sets the raw value
+		float toFloat() const;			// Converts to float
+		int toInt() const;				// Converts to int
 };
 
 std::ostream& operator<<(std::ostream& os, const Fixed& fixed); // INSERTION operator
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/11 17:08:39 by julcalde          #+#    #+#             */
-/*   Updated: 2025/08/11 17:34:58 by julcalde         ###   ########.fr       */
+/*   Created: 2025/08/11 17:45:07 by julcalde          #+#    #+#             */
+/*   Updated: 2025/08/11 17:47:04 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,14 @@
 int main(void)
 {
 	Fixed		a;
-	Fixed const	c(42.42f);
-	Fixed const	b(10);
-	Fixed const	d(b);
+	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
 	
-	a = Fixed(1234.4321f);
-	
-	std::cout << "\n" << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << "\n" << std::endl;
-	
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << "\n" << std::endl;
-	
+	std::cout << "\n" << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
 	return (0);
 }
